0059391f6ac6b3f3bcd59a5484eb1fc0a65e6042
Atal Prateek Barla <prateek.atal@gmail.com>
2026-02-18 12:04:53 +0530

    s

--- application/helpers/redirect_helper.php
+++ application/helpers/redirect_helper.php
@@ -34,7 +34,7 @@
 	if(!function_exists('loginredirect')) {
   		function loginredirect($url='home/') {
     		$CI = get_instance();
-			if($CI->session->user!==NULL && $CI->session->role=='admin' && $CI->session->project==PROJECT_NAME){
+			if($CI->session->user!==NULL && $CI->session->project==PROJECT_NAME){
 				if($CI->session->redirecturl!=NULL) {
 					$redirecturl=$CI->session->redirecturl;
 					$CI->session->unset_userdata('redirecturl');
